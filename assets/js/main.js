ee92d19d63c97b846b819b205b332334dfc279c4
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-02-29 05:54:56 +0100

    Add logic to math functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,18 +9,32 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked submit!")
             }else{
                 let gameType=this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 /**
  * the main function to keep game running and it generates two random numbers 
  * to be used later in the code
  */
-function runGame(){
+function runGame(gameType){
     let num1=Math.floor(Math.random()*25)+1;
     let num2=Math.floor(Math.random()*25)+1;
+
+    if(gameType=="addition"){
+        displayAdditionQuestion(num1,num2);
+    }else if(gameType=="subtraction"){
+        displaySubtractQuestion(num1,num2);
+    }else if(gameType=="multiply"){
+        displayMultiplyQuesiton(num1,num2);
+    }else if(gameType=="division"){
+        dispolayDivisionQuestion(num1,num2);
+    }else{
+        alert(`Unknown Game Type ${gameType}`);
+        throw (`Aborting ...`);
+    }
 }
 function checckAnswer(){
 
@@ -36,15 +50,25 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent='+';
 
 }
-function displaySubtractQuestion(){
-
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent='-';
 }
-function displayMultiplyQuesiton(){
-
+function displayMultiplyQuesiton(operand1, operand2){
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent='x';
 }
-function dispolayDivisionQuestion(){
-
+function dispolayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent='/';
 }
